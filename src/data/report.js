c27227cc7e50abe807b7c1b9a5157f205585c6bf
tuantv <tuanhanhphuc123@gmail.com>
2020-07-16 03:08:37 +0700

    update chart my report, fake data mockup

---
+++ src/data/report.js
@@ -0,0 +1,21 @@
+export default [
+  {
+    id: 1,
+    date: '2020/07/12',
+    projects: [
+      { id: 1, name: 'Project 1', color: '#2db7f5', percent: 50 },
+      { id: 2, name: 'Project 4', color: '#f50', percent: 50 },
+    ]
+  },
+  {
+    id: 2,
+    date: '2020/07/13',
+    projects: [
+      { id: 1, name: 'Project 2', color: '#87d068', percent: 50 },
+      { id: 2, name: 'Project 4', color: '#f50', percent: 50 },
+    ]
+  },
+  { id: 3, date: '2020/07/14', projects: [{ id: 1, name: 'Project 3', color: '#108ee9', percent: 100 }] },
+  { id: 4, date: '2020/07/15', projects: [{ id: 1, name: 'Project 4', color: '#f50', percent: 100 }] },
+  { id: 5, date: '2020/07/16', projects: [{ id: 1, name: 'Project 1', color: '#2db7f5', percent: 100 }] },
+];
