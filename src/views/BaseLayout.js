8a2dc8d28dbd0ca9f0adc39820350ad73fd14380
tuantv <tuanhanhphuc123@gmail.com>
2020-07-16 02:30:59 +0700

    fix selectedKeys Menu

--- src/views/BaseLayout.js
+++ src/views/BaseLayout.js
@@ -15,7 +15,8 @@ import pjson from '../../package.json';
 const { Sider, Content } = Layout;
 
 export default ({ children }) => {
-  const match = useRouteMatch();
+  const { path } = useRouteMatch();
+  const selectedKeys = path && path !== '/' ? path : routes.PROJECT;
 
   return (
     <Layout className="app-layout">
@@ -26,7 +27,7 @@ export default ({ children }) => {
         <Menu
           theme="light"
           mode="inline"
-          selectedKeys={[match.path || routes.PROJECT]}
+          selectedKeys={selectedKeys}
         >
           <Menu.Item key={routes.PROJECT} icon={<ProjectOutlined />}>
             <Link to={routes.PROJECT}>Project</Link>
